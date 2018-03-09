84bceb8625b41718d525c41427c886cd9c119346
Dennis van Zanten <dennisu64@gmail.com>
2018-03-09 08:50:37 +0100

    added some testcode

--- public/views/layout.php
+++ public/views/layout.php
@@ -6,6 +6,9 @@
 		<link rel="stylesheet" href="../assets/css/main.css" />
 	</head>
 	<body>
+		<nav>
+			<div class="nav"></div>
+		</nav>
 		This is the body.
 	</body>
 	<script type="text/javascript" src="../assets/js/main.js"></script>
